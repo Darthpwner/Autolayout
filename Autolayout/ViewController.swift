0678772198c9494fe2715ccc92260ee67b5f37cf
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-09-04 01:18:48 -0700

    Set up some of the autolayout margins

---
+++ Autolayout/ViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ViewController.swift
+//  Autolayout
+//
+//  Created by Matthew Allen Lin on 9/4/15.
+//  Copyright (c) 2015 Matthew Allen Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {}
+
